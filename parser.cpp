e43427fa982c065bafa5bb507cfeb98db918509a
DeckerSU <support@decker.su>
2020-03-11 19:28:28 +0300

    add single thread password check algo

--- parser.cpp
+++ parser.cpp
@@ -20,7 +20,13 @@
 #include "utilstrencodings.h"
 #include "tinyformat.h"
 
+#include <fstream>
+#include <chrono>
+
 using namespace rapidjson;
+
+#include <bitcoin/system.hpp>
+using namespace libbitcoin::system;
 
 /* changeable params */
 static const std::size_t MAX_SENDMANY_OUTPUTS = 10;
@@ -38,6 +44,9 @@ static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
 static const CAmount MAX_MONEY = 21000000 * COIN;
+
+static const std::string CL_GREEN = "\x1b[01;32m";
+static const std::string CL_NORMAL = "\x1B[0m";
 
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
@@ -82,6 +91,14 @@ void PrintSendManyCli(std::ostream& ostr, const CSendManyOutput &vec, int64_t tx
             ostr << std::endl;
         }
     }
+}
+
+// https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
 }
 
 int main()
@@ -132,58 +149,92 @@ int main()
 
     std::cerr << "Ok!" << std::endl;
 
-    // need to sort map by amounts before iterate
-    CAmount total = 0, total_skipped = 0; int64_t txcount = 0, outcount = 0;
-    CSendManyOutput vSendManyOutput;
-
-    typedef std::function<bool(CBalanceRecord, CBalanceRecord)> Comparator;
-    // Defining a lambda function to compare two pairs. 
-    Comparator compFunctor =
-        [](CBalanceRecord elem1, CBalanceRecord elem2)
+    // https://stackoverflow.com/questions/7868936/read-file-line-by-line-using-ifstream-in-c
+    std::ifstream input( "dict.txt" );
+    std::set<std::string> setPasswords;
+    
+    {
+        // read dictionary
+        auto start = std::chrono::steady_clock::now();
+        for( std::string line; getline( input, line ); )
         {
-            //return (elem1.second > elem2.second);
-            
-            // First sort by balance ...
-            if (elem1.second > elem2.second) return true;
-            if (elem1.second < elem2.second) return false;
-            // ... then by address, ...
-            if (elem1.first > elem2.first) return true;
-            if (elem1.first < elem2.first) return false;
+            rtrim(line);
+            setPasswords.insert(line);
+        }
+        auto end = std::chrono::steady_clock::now();
 
-        };
-
-    // Declaring a set that will store the pairs using above comparision logic
-    std::set<CBalanceRecord, Comparator> setOfBalances(
-            mapBalances.begin(), mapBalances.end(), compFunctor);
-    
-    assert(mapBalances.size() == setOfBalances.size());
-
-    //for (const auto& kv : mapBalances) {
-    for (const CBalanceRecord& kv : setOfBalances) {
-
-       if (vSendManyOutput.size() == MAX_SENDMANY_OUTPUTS) {
-           // output komodo-cli sendmany
-           txcount++; outcount += vSendManyOutput.size();
-           PrintSendManyCli(std::cout, vSendManyOutput, txcount, useExtendedScript);
-           vSendManyOutput.clear();
-       }
-       //vSendManyOutput.push_back( std::make_pair(kv.first, kv.second) );
-       if (!setExcludeAddresses.count(kv.first)) {
-           vSendManyOutput.emplace_back(kv.first, kv.second);
-           total += kv.second;
-       }
-       else
-        total_skipped += kv.second;
+        std::cerr << "Total passwords: " << setPasswords.size()
+            << " (" << (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()) << " ms)" << std::endl;
     }
 
-    if (vSendManyOutput.size() > 0) {
-        txcount++; outcount += vSendManyOutput.size();
-        PrintSendManyCli(std::cout, vSendManyOutput,txcount, useExtendedScript);
+    ec_secret privkey;
+    ec_compressed pubkey;
+    one_byte addr_prefix;
+    data_chunk prefix_pubkey_checksum;
+    std::string kmd_addr;
+
+    int64_t n = 0;
+    auto start = std::chrono::steady_clock::now();
+
+    for (std::set<std::string>::const_iterator iter = setPasswords.begin(); iter != setPasswords.end(); ++iter) {
+
+        std::string passphrase = *iter;
+
+        std::vector<char> passphrase_bytes(passphrase.begin(), passphrase.end());
+        auto passphrase_data_slice = data_slice((const uint8_t *)passphrase_bytes.data(),(const uint8_t *)(passphrase_bytes.data() + passphrase_bytes.size()));
+        auto sha256sum = sha256_hash(passphrase_data_slice);
+
+        sha256sum[0]  = sha256sum[0] & 248;
+        sha256sum[31] = sha256sum[31] & 127;
+        sha256sum[31] = sha256sum[31] | 64;
+
+        //auto sha256sum_hex = encode_base16(sha256sum);
+        //std::cerr << passphrase << " - " << sha256sum_hex << std::endl;
+        //decode_base16(privkey, sha256sum_hex);
+        //secret_to_public(pubkey, privkey);
+        privkey = sha256sum;
+
+        secret_to_public(pubkey,privkey);
+        std::string pubkeyhex_str = encode_base16(pubkey);
+
+        // Pubkeyhash: sha256 + hash160
+        auto my_pubkeyhash = bitcoin_short_hash(pubkey);
+
+        addr_prefix = { { 60 } };
+        // Byte sequence = prefix + pubkey + checksum(4-bytes)
+        prefix_pubkey_checksum = to_chunk(addr_prefix);
+        extend_data(prefix_pubkey_checksum, my_pubkeyhash);
+        append_checksum(prefix_pubkey_checksum);
+        // Base58 encode byte sequence -> Bitcoin Address
+        kmd_addr = encode_base58(prefix_pubkey_checksum);
+
+        if (n % 500000 == 0) {
+            auto end = std::chrono::steady_clock::now();
+            std::cerr << "[" << n << "/" << setPasswords.size() << "] "
+                << "" << (std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()) << " ms" << std::endl;
+            // std::cerr << "mapBalances.size() = " << mapBalances.size() << std::endl;
+            start = end;
+        }
+
+
+        // if (mapBalances[kmd_addr] != 0) // (!) never use std::map [] construction for reading, it creates element in map (!)
+        auto it = mapBalances.find(kmd_addr);
+        if ( it != mapBalances.end())
+        {
+            one_byte secret_prefix = { { 188 } };
+            one_byte secret_compressed = { { 0x01 } }; // omitted if uncompressed
+            // Apply prefix, suffix & append checksum
+            auto prefix_secret_comp_checksum = to_chunk(secret_prefix);
+            extend_data(prefix_secret_comp_checksum, privkey);
+            extend_data(prefix_secret_comp_checksum, secret_compressed);
+            append_checksum(prefix_secret_comp_checksum);
+
+            std::string kmd_wif = encode_base58(prefix_secret_comp_checksum);
+            std::cout << passphrase << " - " << kmd_addr << " (" << kmd_wif << ") - " << CL_GREEN << ValueFromAmount((*it).second) << CL_NORMAL << std::endl;
+        }
+
+        n++;
     }
-    
-    std::cerr << "Total: " << std::endl;
-    std::cerr << "------ " << ValueFromAmount(total) << " coins sent to " << outcount << " addresses in " << txcount << " txes." << std::endl;
-    std::cerr << "------ " << ValueFromAmount(total_skipped) << " coins filtered (skipped) from " << setExcludeAddresses.size() << " addresses." << std::endl;
 
     return 0;
 }
